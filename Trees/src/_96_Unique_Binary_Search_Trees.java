b785559d81a7c993219cf266004dc43cd60160a6
nikhilmeena10 <nikhil.meena.civ14@itbhu.ac.in>
2020-02-06 18:06:55 +0530

    Number of unique binary search trees

---
+++ Trees/src/_96_Unique_Binary_Search_Trees.java
@@ -0,0 +1,16 @@
+public class _96_Unique_Binary_Search_Trees {
+
+    //Catalan numbers!!
+    //https://www.youtube.com/watch?v=YDf982Lb84o&list=PLrmLmBdmIlpsHaNTPP_jHHDx_os9ItYXr&index=37
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        return dp[n];
+    }
+}
