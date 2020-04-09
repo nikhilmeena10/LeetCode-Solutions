2947884d917b86fdfbe019836aa366cb2e90e341
nikhilmeena10 <nikhil.meena.civ14@itbhu.ac.in>
2020-04-09 23:44:39 +0530

    KJ -> Subarray Product less than K

---
+++ Arrays/src/_713_Subarray_Product_Less_Than_K.java
@@ -0,0 +1,14 @@
+public class _713_Subarray_Product_Less_Than_K {
+
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) return 0;
+        int prod = 1, ans = 0, left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            prod *= nums[right];
+            while (prod >= k) prod /= nums[left++];
+            ans += right - left + 1;
+        }
+        return ans;
+    }
+
+}
