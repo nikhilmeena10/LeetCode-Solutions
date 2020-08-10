c9054a39b566d39c842d12e0dd51003e2d2b7317
nikhilmeena10 <nikhil.meena.civ14@itbhu.ac.in>
2020-08-10 23:26:43 +0530

    Extra comments

--- AugustLeetCodeChallenge/src/_10.java
+++ AugustLeetCodeChallenge/src/_10.java
@@ -1,5 +1,8 @@
 public class _10 {
 
+    //also see alternate solution in discuss. What if length was not known previously?
+    //remember result = result*26 + s.charAt(i)-'A'-1 going from 0 to ....
+    //similar to converting binary to decimal when length is not known before
     public int titleToNumber(String s) {
         int len = s.length();
         int pow = len - 1;
