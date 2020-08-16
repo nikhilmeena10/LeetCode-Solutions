e01ad1a08ba8189e9b4feda94d38bcbb55721e06
nikhilmeena10 <nikhil.meena.civ14@itbhu.ac.in>
2020-08-16 16:13:01 +0530

    221

---
+++ DynamicProgramming/src/_221_Maximal_Square.java
@@ -0,0 +1,21 @@
+public class _221_Maximal_Square {
+
+    //after looking at discussion
+    //check how 1D dp will work for this
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0) return 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+        int result = 0;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    result = Math.max(result, dp[i][j]);
+                }
+            }
+        }
+        return result * result;
+    }
+}
